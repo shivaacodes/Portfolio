1fcccd7902fb47e90372a127c291be9a1a7e6a68
shivaacodes <shivasajay007@gmail.com>
2024-08-12 13:54:53 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Components/Header";
+import Hero from "./Components/Header";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Skills.jsx
@@ -0,0 +1,5 @@
+function Skills() {
+  return <div></div>;
+}
+
+export default Skills;
